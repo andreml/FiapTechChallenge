8cda3b2cd95834d0cedcc7bdf3b24399a41d7676
andremunizdelima <andremunizdelima@hotmail.com>
2023-08-29 15:15:53 -0300

    Add project files.

---
+++ FiapTechChallenge/CallApi.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace FiapTechChallenge
+{
+    public class CallApi
+    {
+        public async Task<string> GetKey(string key, HttpClient client, Uri uri)
+        {
+            string jsonBody2 = "{\"key\":\"" + key + "\"}";
+            HttpContent content = new StringContent(jsonBody2, Encoding.UTF8, "application/json");
+            var content1 = client.PostAsync(uri, content).Result;
+
+            if ((int)content1.StatusCode >= 204) Console.Write(".");
+            
+            Console.WriteLine($"Chave encontrada: {key}");
+            return await Task.FromResult($"Chave encontrada: {key}");
+        }
+    }
+}
---
+++ FiapTechChallenge/Program.cs
@@ -0,0 +1,37 @@
+﻿using FiapTechChallenge;
+
+//string[] acentosMinusculas = { "á", "à", "â", "ã", "é", "ê", "í", "ó", "ô", "õ", "ú", "ü" };
+//string[] acentosMaiusculas = { "Á", "À", "Â", "Ã", "É", "Ê", "Í", "Ó", "Ô", "Õ", "Ú", "Ü" };
+
+//string[] minusculas = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+//string[] maiusculas = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+string[] alfabetoCompleto = { "á", "à", "â", "ã", "é", "ê", "í", "ó", "ô", "õ", "ú", "ü", "Á", "À", "Â", "Ã", "É", "Ê", "Í", "Ó", "Ô", "Õ", "Ú", "Ü", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+var _ = new CallApi();
+var client = new HttpClient();
+var uri = new Uri("https://fiap-inaugural.azurewebsites.net/fiap");
+client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "application/json");
+
+var tasks = new List<Task>();
+var options = new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount };
+
+//Parallel.For(1, 10001, options, index =>
+//{
+//    Parallel.ForEach(alfabetoCompleto, letra =>
+//    {
+//        string key = string.Concat(letra, index.ToString());
+//        tasks.Add(new Task(() => CallApi.GetKey(key, client, uri)));
+//    });
+//});
+
+Parallel.For(1, 11, options, index =>
+{
+    foreach (var letra in alfabetoCompleto)
+    {
+        string key = string.Concat(letra, index.ToString());
+        tasks.Add(new Task(async () => await _.GetKey(key, client, uri)));
+    };
+});
+
+await Task.WhenAll(tasks);
